39237f035b80310f20de77f913d64b100c65c9a1
jl90 <leonardjason90.jl@gmail.com>
2015-01-15 09:41:18 -0700

    Tweak pegasusQ freq for moto_msm8960

--- drivers/cpufreq/cpufreq_pegasusq.c
+++ drivers/cpufreq/cpufreq_pegasusq.c
@@ -57,7 +57,7 @@
 #define DEF_START_DELAY				(0)
 
 #define UP_THRESHOLD_AT_MIN_FREQ		(40)
-#define FREQ_FOR_RESPONSIVENESS			(1728000)
+#define FREQ_FOR_RESPONSIVENESS			(1512400)
 /* for fast decrease */
 #define FREQ_FOR_FAST_DOWN			(1190400)
 #define UP_THRESHOLD_AT_FAST_DOWN		(90)
